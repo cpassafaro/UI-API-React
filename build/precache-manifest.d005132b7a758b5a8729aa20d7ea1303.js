7cf26252a818dcb6159a05cb0a19b937ce5abf80
christinapassafaro <christinapassafaro@gmail.com>
2020-12-01 07:49:04 -0500

    build

---
+++ build/precache-manifest.d005132b7a758b5a8729aa20d7ea1303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dadd59b41191e43398ea229b7432eb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "064054bf5d043797bdf3",
+    "url": "/static/css/main.1748f0f5.chunk.css"
+  },
+  {
+    "revision": "8ff02f0811ad688b5b49",
+    "url": "/static/js/2.79929ac9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.79929ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064054bf5d043797bdf3",
+    "url": "/static/js/main.c6d0a657.chunk.js"
+  },
+  {
+    "revision": "13502922d173dea31983",
+    "url": "/static/js/runtime-main.14cc7e85.js"
+  }
+]);
